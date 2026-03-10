e0dcbe72d5bc6d82be47725cb9d88537e0abbced
Unbreakableb <benjaminzhu6@gmail.com>
2026-03-10 15:29:52 -0400

    Add files via upload

--- Module-5-user post/index.js
+++ Module-5-user post/index.js
@@ -1,24 +1,28 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-
+const userListElement = document.querySelector(".user-list");
 
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users")
     const usersData = await users.json();
-    const userListElement = document.querySelector(".user-list");
+    console.log(usersData);
+    userListElement.innerHTML = usersData.map((user) => userHTML(user)).join("");
+}
 
-    userListElement.innerHTML = usersData
-    .map(
-      (user) =>`<div class="user-card">
+function showUserPosts(user){
+  console.log(user);
+}
+
+function userHTML(user){
+  return `<div class="user-card" onclick="showUserPosts(${user.id})>
             <div class="user-card__container">
-              <h3>User's Name</h3>
-                <p><b>Email:</b> email@email.com</p>
-                <p><b>Phone:</b> 0000000000</p>
-                <p><b>Website:</b> <a href="https://website.website" 
-                target="_blank">website.website</a></p>
+              <h3>${user.name}</h3>
+                <p><b>Email:</b> ${user.email}</p>
+                <p><b>Phone:</b> ${user.phone}</p>
+                <p><b>Website:</b> <a href="https://${user.website}" 
+                target="_blank">${user.website}</a></p>
             </div>
-          </div>`)
-      .join("");
+          </div>`
 }
 
 window.addEventListener("DOMContentLoaded", function(){
